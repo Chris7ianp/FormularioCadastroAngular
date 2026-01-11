b39c3cff37c356e6a54af582a989d4bfe49bf410
Chris7ianp <christian.paulo.rj@gmail.com>
2026-01-11 18:35:10 -0300

    meu primeiro commit

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule, MatIcon } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [FlexLayoutModule, MatCardModule, FormsModule, MatFormFieldModule, MatInputModule, MatIcon, MatButtonModule, MatIconModule],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss'
+})
+export class CadastroComponent {
+
+}
