77e86301a7ca824fdf5b6b262c7c4aed2fd016ba
Chris7ianp <christian.paulo.rj@gmail.com>
2026-01-12 22:02:59 -0300

    implementando o service e utilizando o localStorage.

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -6,6 +6,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatIconModule, MatIcon } from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import { Cliente } from './cliente';
+import { ClienteService } from '../cliente.service';
 
 
 @Component({
@@ -16,4 +18,13 @@ import {MatButtonModule} from '@angular/material/button';
 })
 export class CadastroComponent {
 
+    cliente: Cliente = Cliente.newCliente();
+
+    constructor(private service: ClienteService){
+      
+    }
+
+    salvar(){
+        this.service.salvar(this.cliente);
+    }
 }
